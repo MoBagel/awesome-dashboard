c6b91eca7b121bce739aa405280c937ba9a546d5
Ianpig <chu2815@hotmail.com>
2022-01-05 13:49:12 +0800

    Fix type warning

--- src/NotificationBar/stories/NofificationBar.stories.tsx
+++ src/NotificationBar/stories/NofificationBar.stories.tsx
@@ -1,4 +1,4 @@
-import { Story, Meta } from '@storybook/react';
+import type { Story, Meta } from '@storybook/react';
 import React from 'react';
 import { NotificationBar } from '../NotifticationBar';
 import type { NotificationBarProps } from '../NotifticationBar';
