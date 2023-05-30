182953d819cc30d2df006af7bcb4992fe191efd4
ian <chu2815@hotmail.com>
2023-05-30 18:44:02 +0800

    Add lang select could use  to extend (#14)
    Co-authored-by: ian <ian@mobagel.com>

--- src/RightContent/SelectLang.tsx
+++ src/RightContent/SelectLang.tsx
@@ -4,6 +4,7 @@ import { Menu, Space } from 'antd';
 import type { MenuInfo } from 'rc-menu/lib/interface';
 import HeaderDropdown from './HeaderDropdown';
 import styles from './index.less';
+import type { extendLangsDropdownProps } from './HeaderRight';
 
 const DEFAULT_LANGS = [
   {
@@ -12,20 +13,21 @@ const DEFAULT_LANGS = [
   },
   {
     lang: 'zh-TW',
-    label: '繁體中文'
-  }
+    label: '繁體中文',
+  },
 ];
 
-const SelectedLang: React.FC<{ onUpdateLocale: (lang: string, realReload?: boolean) => void }> = ({
-  onUpdateLocale
-}) => {
+const SelectedLang: React.FC<{
+  extendLangsDropdown?: extendLangsDropdownProps[];
+  onUpdateLocale: (lang: string, realReload?: boolean) => void;
+}> = ({ onUpdateLocale, extendLangsDropdown = [] }) => {
   const itemClick = (item: MenuInfo) => {
     onUpdateLocale(item?.key, false);
   };
 
   const menu = (
     <Menu onClick={itemClick}>
-      {DEFAULT_LANGS.map((lang) => (
+      {[...DEFAULT_LANGS, ...extendLangsDropdown].map((lang) => (
         <Menu.Item key={lang?.lang}>
           <Space>{lang.label}</Space>
         </Menu.Item>
@@ -34,7 +36,7 @@ const SelectedLang: React.FC<{ onUpdateLocale: (lang: string, realReload?: boole
   );
 
   return (
-    <HeaderDropdown overlay={menu} placement='bottomCenter'>
+    <HeaderDropdown overlay={menu} placement="bottomCenter">
       <span className={styles.action}>
         <GlobalOutlined />
       </span>
